1878f17b812d4cc99cc04eb5f7e8388cf0bec682
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-02-13 14:38:14 -0500

    primeras 2 funciones queue

---
+++ DataStructures/Queue/queue.py
@@ -0,0 +1,8 @@
+from DataStructures.List import array_list as al
+def peek(my_queue):
+    first = al.first_element(my_queue)
+    return first
+def is_empty(my_queue):
+    x = al.is_empty(my_queue)
+    return x
+
