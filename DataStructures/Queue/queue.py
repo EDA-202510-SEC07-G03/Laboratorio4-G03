329fd478e7a9ffe8d5ee50a44bb2e562ff220d7d
SamuelVillamil9313 <s.villamila@uniandes.edu.co>
2025-02-13 14:49:19 -0500

    New queue and Enqueue

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -13,12 +13,12 @@ def dequeue(my_queue):
 
 def size(dequeue):
     return dequeue["size"]
-import DataStructures.List.array_list as ar
+
 
 def new_queue():
-    return ar.new_list
+    return al.new_list
 
 def enqueue(my_queue,element):
     
-    nueva_cola=ar.add_last(my_queue,element)
+    nueva_cola=al.add_last(my_queue,element)
     return nueva_cola
